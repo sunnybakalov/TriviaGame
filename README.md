33451f26471a64c3ab3f90a067319f8da7370d86
Stan Bakalov <sunny.bakalov@gmail.com>
2018-11-18 19:13:38 -0700

    object created

--- README.md
+++ README.md
@@ -7,25 +7,25 @@ Your basketball knowledge will be tested in this trivia game.
 //Which of these players has won the most championships?
     //Michael Jordan
     //Kobe Bryant
-    //Bill Russell
+    //Bill Russell *
     //Kevin Durant
 
 //Which of these players has only made one 3 point shot in their entire career?
     //Kemba Walker
-    //Shaquille O'Neal
+    //Shaquille O'Neal *
     //Klay Thompson
     //Rudy Gobert
 
 //Which team has never won an NBA championship?
     //Cleveland Cavaliers
     //Detroit Pistons
-    //Utah Jazz
+    //Denver Nuggets *
     //New York Knicks
 
 //Which of these players has the most 60 point games?
     //Kareem Abdul Jabbar
     //Kobe Bryant
-    //Wilt Chamberlain
+    //Wilt Chamberlain*
     //Larry Bird
 
 //
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,34 @@
-//
+//variables needed:
+//object needed for the answer to each question.
+var answer = {
+    russell: {
+        name: "Bill Russell",
+        fact: "Russell won 11 championships with the Boston Celtics during his 13 year long playing career.",
+        image: '/images/russell.jpg'
+    },
+    shaq: {
+        name: "Shaquille O'Neal",
+        fact: "Shaq only made 1 three point shot in his entire career!",
+        image: '/images/shaq.jpg'
+    },
+    nuggets: {
+        name: "Denver Nuggets",
+        fact: "The Nuggets are 1 of 13 NBA teams that have not won an NBA championship.",
+        image: '/images/nuggets.jpg'
+    },
+    wilt: {
+        name: "Wilt Chamberlain",
+        fact: "Wilt Chamberlain had a total of 32 60-point games. The #2 spot for the most 60 point games goes to Kobe Bryant with 6 games.",
+        image: '/images/wilt.jpg'
+    },
+}
+
+//variable for the user's choice(click)
+//variable for correct choices
+
+//slides
+//need to have an on click function for the answer that the user chooses on each slide
+//if statement on each slide. if (userChoice === rightAnswer) {$().html("Correct!") }
+
+//there needs to be a 30 second countdown function that can be applied to each slide
+
